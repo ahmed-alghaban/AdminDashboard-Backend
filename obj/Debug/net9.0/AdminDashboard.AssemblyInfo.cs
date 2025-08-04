632bd18e4c95197556e596a691365c986e284998
ahmed-alghaban <a.alghaban@outlook.com>
2025-08-04 18:14:08 +0300

    add health endpoint to check

--- obj/Debug/net9.0/AdminDashboard.AssemblyInfo.cs
+++ obj/Debug/net9.0/AdminDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16fd3d72af8275105a879082243c719fd52924bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85dc5bc0265753f915b053674f755f162d92b274")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
