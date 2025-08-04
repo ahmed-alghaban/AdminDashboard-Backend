2794d5678a6074d5fe6196762c3ff84e10a64288
ahmed-alghaban <a.alghaban@outlook.com>
2025-08-05 01:14:21 +0300

    docker files

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+﻿# -------- Build stage --------
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
+WORKDIR /src
+COPY AdminDashboard.csproj ./
+RUN dotnet restore AdminDashboard.csproj
+COPY . ./
+RUN dotnet publish AdminDashboard.csproj -c Release -o /app/publish
+
+# -------- Runtime stage --------
+FROM mcr.microsoft.com/dotnet/aspnet:9.0
+WORKDIR /app
+COPY --from=build /app/publish .
+# Listen on 5000 inside the container (EB routes to this)
+ENV ASPNETCORE_URLS=http://0.0.0.0:5000
+EXPOSE 5000
+ENTRYPOINT ["dotnet", "AdminDashboard.dll"]
--- obj/Debug/net9.0/AdminDashboard.AssemblyInfo.cs
+++ obj/Debug/net9.0/AdminDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+632bd18e4c95197556e596a691365c986e284998")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+822535b51337bb85d2795e6aaa443d136b8232aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
